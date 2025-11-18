0755b6759dd56f194fbec0c7de09190971e31028
maritzaadeliaa <maritzaadelia076@gmail.com>
2025-11-18 22:46:35 +0700

    Create erendis.sh

---
+++ scripts/erendis.sh
@@ -0,0 +1,10 @@
+# File: /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+    address 10.70.0.200
+    netmask 255.255.255.192
+    gateway 10.70.0.193
+    dns-nameservers 192.168.122.1 8.8.8.8
